b9018a552c4b51d31fc1ccc20fec2ffc58296426
jitenpanchal1 <jeetenpanchal84@gmail.com>
2025-08-24 12:09:33 +0530

    Hotel site

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -23,7 +23,7 @@ function Contact() {
                                 <MDBIcon icon='map-marker-alt' className='me-3 text-warning' size='lg' />
                                 <div>
                                     <h5 className='fw-bold text-dark'>Our Location</h5>
-                                    <p className='mb-0 text-muted'>123 Gourmet Avenue, Foodie District<br />Metro City, MC 12345</p>
+                                    <p className='mb-0 text-muted'>C-403 Vandematram icon, Gota<br />Ahmedabad, gujarat 382481</p>
                                 </div>
                             </div>
 
@@ -31,7 +31,7 @@ function Contact() {
                                 <MDBIcon icon='phone-alt' className='me-3 text-success' size='lg' />
                                 <div>
                                     <h5 className='fw-bold text-dark'>Call Us</h5>
-                                    <p className='mb-0 text-muted'>Reservations: (123) 456-7890<br />General: (123) 456-7891</p>
+                                    <a href='tel:+918490951618' className='mb-0 text-muted'>Reservations: +91 8490951618</a>
                                 </div>
                             </div>
 
@@ -39,7 +39,8 @@ function Contact() {
                                 <MDBIcon icon='envelope' className='me-3 text-danger' size='lg' />
                                 <div>
                                     <h5 className='fw-bold text-dark'>Email Us</h5>
-                                    <p className='mb-0 text-muted'>reservations@wfd.com<br />info@wfd.com</p>
+                                    <a className='mb-0 text-muted'
+                                        href="mailto:jeetenpanchal84@gmail.com">jeetenpanchal84@gmail.com</a>
                                 </div>
                             </div>
 
@@ -69,11 +70,7 @@ function Contact() {
                     <MDBCol md='6' className='my-auto'>
                         <div className='  shadow-3 rounded-4 bg-white'>
                             <div className='ratio ratio-16x9 rounded-3 overflow-hidden mb-4'>
-
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3670.3182646635405!2d72.54854027454196!3d23.085442413980584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e8385d3838af5%3A0x4037e43c579f2a0f!2sKumkum%20party%20plot!5e0!3m2!1sen!2sin!4v1750244135367!5m2!1sen!2sin" width="600" height="450" allowFullScreen
-                                    loading='lazy'
-                                    referrerPolicy='no-referrer-when-downgrade'
-                                    title='Restaurant Location'></iframe>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14679.980296134572!2d72.53601863807539!3d23.097276165939544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e8318f77825b3%3A0x86b70c30e0395a31!2sNIRMAN%20HOMES%2C%2022%2C%20NR%20Vandematram%20Cross%20Rd%2C%20Gota%2C%20Ahmedabad%2C%20Gujarat%20382481!5e0!3m2!1sen!2sin!4v1756017174940!5m2!1sen!2sin" width="600" height="450" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
                             </div>
                         </div>
                     </MDBCol>
